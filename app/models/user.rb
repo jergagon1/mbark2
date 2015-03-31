7f4bb508ac1478963a62d637caf27bb0819904ad
Jeremy Gagon <jergagon1@gmail.com>
2015-03-31 12:06:25 -0700

    add controllers and authentication in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,31 @@
 class User < ActiveRecord::Base
+  include BCrypt
   has_many :dogs
   has_many :mbarks
   has_many :events, through: :mbarks
 
-  validates :email, :password, presence: true
-  validates :password, length: { minimum: 6 }
-  validates :email, uniqueness: true
+  attr_accessor :password
+  before_save :encrypt_password
 
-  include BCrypt
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
 
-  def password
-    @password ||= Password.new(password_hash) if password_hash
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
   end
 
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
-
-  def authenticate(password)
-    self.password == password
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
   end
 
 end
---
+++ development_notes.md
@@ -0,0 +1,38 @@
+Stretch Goals
+
+twitter oauth
+-login via twitter
+-tweet events
+
+google maps
+-show event location
+
+
+Routes
+
+root
+
+not-signed in
+get '/'
+present login or signup
+
+
+signed-in
+single page
+profile
+-pop-up show
+-pop-up edit
+events
+-pop-up create event
+-button join event
+
+
+site/username
+
+/
+-show events
+
+/username
+-show profile
+
+
