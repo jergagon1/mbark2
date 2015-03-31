d25c3e344490ca95808be7ca4d43354d7f0caa0c
Jeremy Gagon <jergagon1@gmail.com>
2015-03-30 19:23:26 -0700

    Add default value to dog image field

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,12 @@
 class Dog < ActiveRecord::Base
   belongs_to :user
+  before_save :default_values
+
+  validates :name, presence: true
+  validates :description, presence: true
+
+  def default_values
+    self.image ||= "http://www.cdc.gov/animalimportation/images/dog2.jpg"
+  end
+
 end
