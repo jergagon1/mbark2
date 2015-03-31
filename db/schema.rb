ec81206777d9a1ae4133fc6f53d775257eae5166
Jeremy Gagon <jergagon1@gmail.com>
2015-03-30 19:16:30 -0700

    generate dog model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331015345) do
+ActiveRecord::Schema.define(version: 20150331021411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dogs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.string   "breed"
+    t.string   "size"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "dogs", ["user_id"], name: "index_dogs_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -26,4 +39,5 @@ ActiveRecord::Schema.define(version: 20150331015345) do
     t.datetime "updated_at",    null: false
   end
 
+  add_foreign_key "dogs", "users"
 end
