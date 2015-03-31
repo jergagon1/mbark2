35c45e80abf4d290628b1685fb40c2b2f102e987
Jeremy Gagon <jergagon1@gmail.com>
2015-03-30 19:30:59 -0700

    create event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331021411) do
+ActiveRecord::Schema.define(version: 20150331022556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,18 @@ ActiveRecord::Schema.define(version: 20150331021411) do
   end
 
   add_index "dogs", ["user_id"], name: "index_dogs_on_user_id", using: :btree
+
+  create_table "events", force: :cascade do |t|
+    t.date     "date"
+    t.string   "time"
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.text     "description"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
